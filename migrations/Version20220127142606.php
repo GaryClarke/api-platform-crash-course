16e8213e32420183a12e755e4a8529eb059f27a6
Gary Clarke <garyclarke1974@gmail.com>
2022-01-27 16:20:53 +0000

    Initial commit

---
+++ migrations/Version20220127142606.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220127142606 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE manufacturer (
+            id INT AUTO_INCREMENT NOT NULL, 
+            name VARCHAR(255) NOT NULL, 
+            description LONGTEXT NOT NULL, 
+            country_code VARCHAR(3) NOT NULL, 
+            listed_date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE manufacturer');
+    }
+}
---
+++ src/Entity/Manufacturer.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * A manufacturer
+ *
+ * @ORM\Entity
+ */
+#[ApiResource]
+class Manufacturer
+{
+    /**
+     * The id of the manufacturer
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * The name of the manufacturer
+     *
+     * @ORM\Column
+     */
+    private string $name = '';
+
+    /**
+     * The description of the manufacturer
+     *
+     * @ORM\Column(type="text")
+     */
+    private string $description = '';
+
+    /**
+     * The country code of the manufacturer
+     *
+     * @ORM\Column(length=3)
+     */
+    private string $countryCode = '';
+
+    /**
+     * The date that the manufacturer was listed
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private ?\DateTimeInterface $listedDate = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode(): string
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param string $countryCode
+     */
+    public function setCountryCode(string $countryCode): void
+    {
+        $this->countryCode = $countryCode;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getListedDate(): ?\DateTimeInterface
+    {
+        return $this->listedDate;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $listedDate
+     */
+    public function setListedDate(?\DateTimeInterface $listedDate): void
+    {
+        $this->listedDate = $listedDate;
+    }
+}
