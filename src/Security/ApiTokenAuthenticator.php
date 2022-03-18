ed497a3a1d75c15db6e0b470c7dbba54ee9d4283
Gary Clarke <garyclarke1974@gmail.com>
2022-03-18 19:52:40 +0000

    Edit authenticator supports method

--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -30,7 +30,7 @@ class ApiTokenAuthenticator extends AbstractAuthenticator
 
     public function supports(Request $request): ?bool
     {
-        return $request->headers->has('x-api-token');
+        return str_starts_with($request->getPathInfo(), '/api/');
     }
 
     public function authenticate(Request $request): Passport
